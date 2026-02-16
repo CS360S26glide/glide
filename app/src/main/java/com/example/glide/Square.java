10bb16198100e09b498a988ad43baa7610bcf650
alishbajavedkhan <alishbajaved18@gmail.com>
2026-02-17 01:12:35 +0500

    added square shape

---
+++ app/src/main/java/com/example/glide/Square.java
@@ -0,0 +1,11 @@
+package com.example.glide;
+
+public class Square extends Shape {
+    public int x;
+    public int length;
+
+    public Square(int x, int length) {
+        this.x = x;
+        this.length = length;
+    }
+}
