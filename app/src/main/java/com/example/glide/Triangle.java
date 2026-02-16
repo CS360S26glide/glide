cb50735a9909726bd1cbc0d353c6a2b04708a0e3
Sameer-Rahil <27100162@lums.edu.pk>
2026-02-16 23:41:05 +0500

    Add Triangle model

---
+++ app/src/main/java/com/example/glide/Triangle.java
@@ -0,0 +1,13 @@
+package com.example.glide;
+
+public class Triangle extends Shape {
+    public int base;
+    public int height;
+
+    public Triangle(int x, int y, int base, int height) {
+        this.x = x;
+        this.y = y;
+        this.base = base;
+        this.height = height;
+    }
+}
